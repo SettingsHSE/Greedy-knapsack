5ffc946a11dde04e25d7f1b39abef887b57dafd4
Софья Андреева <skandreeva@edu.hse.ru>
2020-04-22 17:09:47 +0300

    fix

---
+++ cmake-build-debug/CMakeFiles/GreedyKnapsack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GreedyKnapsack.dir/main.cpp.obj"
+  "GreedyKnapsack.pdb"
+  "GreedyKnapsack.exe"
+  "GreedyKnapsack.exe.manifest"
+  "libGreedyKnapsack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GreedyKnapsack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
